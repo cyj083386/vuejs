84df2a86d66778f01e2c58c4fd903c76a8e386ba
choi\cyj <cyj083386@gmail.com>
2024-05-29 10:06:09 +0900

    부동산 프로젝트 파일추가

---
+++ vuedongsan/src/App.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="start" :class="{ end : true}">
+      <ModalWindow @closeModal="모달창열렸니 = false" 
+    :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+
+  <div class="menu">
+    <a v-for="a in menu" :key="a">{{a}}</a>
+  </div>
+  <DisCount/> 
+  <ItemCard @openModal="모달창열렸니 = true; 누른거 = $event" :원룸="원룸들[i]" v-for="(a,i) in 원룸들" :key="a"/>
+
+
+  <!-- <ItemCard @openModal="모달창열렸니 = true; 누른거 = $event" :원룸="원룸들[i]" v-for="(a,i) in 원룸들" :key="a"/> -->
+  
+  <!-- <div v-for="(a, i) in 원룸들" :key="i">
+    <img :src="a.image" class = "room-img">
+    <h4 @click="모달창열렸니 = true; 누른거 = i ">{{ a.title }}</h4>
+    <p>{{ a.price }} 원</p>
+  </div> -->
+  <!-- <div v-for="(a, i) in products" :key="i">
+    <img :src="require(`@/assets/images${i}.jpg`)" class = "room-img">
+    <h4 :style="스타일" @click="모달창열렸니 =  true">{{ a }}</h4>
+    <p>{{ price1 }} 만원</p>
+    
+  </div> -->
+  <!-- <div>
+    <h4 :style="스타일">{{ products[0] }} 원룸</h4>
+    <p>{{ price1 }} 만원</p>
+    <button @click="increase">허위매물신고</button> <span>신고수 : {{report}}</span>
+  </div>
+  <div>
+    <h4>{{ products[1] }} 원룸</h4>
+    <p>{{ price2 }} 만원</p>
+  </div> -->
+</template>
+
+<script>
+import data from './components/oneroom.js';
+import ItemCard from './components/ItemCard.vue';
+import DisCount from './components/DisCount.vue';
+
+import ModalWindow from './components/ModalWindow.vue';
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      누른거 : 0,
+      원룸들 : data,
+      모달창열렸니: false,
+      menu : ['home','shop','about'],
+      report : [0, 0, 0],
+      price1 : 60,
+      price2 : 70,
+      스타일 : 'color : blue',
+      products  : ['역삼동 아파트', '천호동 아파트', '마포구 아파트'],
+    }
+  },
+  methods : {
+    increase(i){
+      this.report[i]++
+    }
+  },
+
+  components: {
+    DisCount : DisCount,
+    ModalWindow : ModalWindow,
+    ItemCard: ItemCard,
+  }
+}
+</script>
+
+<style>
+/* .start {
+  opacity: 0;
+  transition: all 1s;
+}
+.end {
+  opacity: 1;
+} */
+
+body {
+  margin: 0;
+}
+div{
+  box-sizing: border-box;
+}
+.black-bg{
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+.discount{
+  background: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+}
+</style>
---
+++ vuedongsan/src/components/ModalWindow.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="black-bg" v-if="모달창열렸니">
+      <div class="white-bg">
+
+        <img :src="원룸들[누른거].image" style="width: 100%;">
+        <DisCount/>
+        <h4>{{ 원룸들[누른거].title }}</h4>
+        <p>{{ 원룸들[누른거].content }}</p>
+        <input v-model.number="month">
+        <form
+        @submit="checkForm"
+        action="https://vuejs.org/"
+        method="post"
+        >
+          <p>
+            <label for="name">Name</label>
+            <input
+              id="name"
+              v-model="name"
+              type="text"
+              name="name"
+            >
+          </p>
+
+          <p>
+            <label for="age">Age</label>
+            <input
+              id="age"
+              v-model="age"
+              type="number"
+              name="age"
+              min="0"
+            >
+          </p>
+
+          <p>
+            <label for="movie">Favorite Movie</label>
+            <select
+              id="movie"
+              v-model="movie"
+              name="movie"
+            >
+              <option>Star Wars</option>
+              <option>Vanilla Sky</option>
+              <option>Atomic Blonde</option>
+            </select>
+          </p>
+
+          <p>
+            <input
+              type="submit"
+              value="Submit"
+            >
+          </p>
+
+        </form>
+        <p>{{ month }}개월 선택함 : {{ 원룸들[누른거].price * month }}</p>
+        <button @click="$emit('closeModal')">닫기</button> 
+      </div>
+  </div>
+</template>
+
+<script>
+// import Vue from 'vue';
+// import SimpleVueValidation from 'simple-vue-validator';
+
+// Vue.use(SimpleVueValidation);
+import DisCount from './DisCount.vue';
+
+export default {
+    name : 'ModalWindow',
+    data(){
+      return{
+        month : 1,
+      }
+    },
+    watch : {
+      month(a){
+        if(typeof(a) != 'number'){
+         alert('숫자를 입력하세요');
+         this.month = 1;
+        }
+        // if(isNaN(a) == true){
+        //   alert('숫자를 입력하세요');
+        //   this.month = 1;
+        // }
+        else if(a >= 13){
+          alert('13이상 입력하지 마세요')
+        }
+      },
+    },
+    components: {
+    DisCount : DisCount,
+    },
+    props:{
+        원룸들 : Array,
+        누른거 : Number,
+        모달창열렸니 : Boolean,
+    }
+}
+</script>
+
+<style>
+
+</style>
